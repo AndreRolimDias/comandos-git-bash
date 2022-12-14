5f3d037f6b17fa73ba598a24e179e1114c6f463f
AndreRolimDias <andrerolimdias_@hotmail.com>
2022-12-13 23:20:56 -0300

    final version

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,7 +1,6 @@
 function copiar() {
-    var texto = document.getElementById('texto')
-    texto.select()
-    document.execCommand('copy')
-    alert('Texto Copiado:' + texto.value)
+  var texto = document.getElementById("texto");
+  texto.select();
+  document.execCommand("copy");
+  alert("Texto Copiado:" + texto.value);
 }
-
