9d9c0ed2bec950cf871bf08e5a07358b07f6b68e
AndreRolimDias <andrerolimdias_@hotmail.com>
2022-12-14 22:55:27 -0300

    added responsiveness

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,5 +1,6 @@
 function copiar() {
-  var texto = document.getElementById("texto");
+  var texto = document.getElementById("texto1");
+  var texto = document.getElementById("texto2");
   texto.select();
   document.execCommand("copy");
   alert("Texto Copiado:" + texto.value);
