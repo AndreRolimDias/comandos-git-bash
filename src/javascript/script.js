06e168160aff4bb95679c31c12316fdb0d68faaa
AndreRolimDias <andrerolimdias_@hotmail.com>
2022-12-18 23:41:28 -0300

    code improvements

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,6 +1,5 @@
 function copiar() {
   var texto = document.getElementById("texto1");
-  var texto = document.getElementById("texto2");
   texto.select();
   document.execCommand("copy");
   alert("Texto Copiado:" + texto.value);
